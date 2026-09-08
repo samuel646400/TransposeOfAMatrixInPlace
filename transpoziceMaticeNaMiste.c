74325c4d05874eaf85b208a8a5c64b42ae60829c
Samuel Matusek <samuel.matusek@gmail.com>
2026-09-08 18:31:28 +0200

    Udelano zakladni nacteni s podminkama a prvni cast alokace

--- transpoziceMaticeNaMiste.c
+++ transpoziceMaticeNaMiste.c
@@ -11,11 +11,21 @@ int transponuj_matici(int ***matice, int *radky, int *sloupce){
 
 int main(void){
 
+    int radky, sloupce;
 
+    printf("Zadej rozmery matice (radky sloupce):\n");
+    if (scanf("%d %d", &radky, &sloupce) != 2 || radky <= 0 || sloupce <= 0){
+        printf("Nespravny vstup.\n");
+        return 1;
+    }
 
+    int **matice = malloc(sizeof(*matice) * radky);
+    if (matice == NULL){
+        printf("Nespravny vstup.\n");
+        return 1;
+    }
 
-
-
+    
 
 
 
