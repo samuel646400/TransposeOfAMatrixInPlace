eb597c61a2ffb10716331b4055cc412440631efd
Samuel Matusek <samuel.matusek@gmail.com>
2026-09-08 18:23:53 +0200

    Zaklad funkci

---
+++ transpoziceMaticeNaMiste.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int transponuj_matici(int ***matice, int *radky, int *sloupce){
+
+
+
+
+    return 1;
+}
+
+int main(void){
+
+
+
+
+
+
+
+
+
+    return 0;
+}
